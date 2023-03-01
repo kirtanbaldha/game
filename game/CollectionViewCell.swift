23f8faac5b455d3aec1db05752afd42846989cb8
kirtan <kirtan9397@gmail.com>
2023-03-01 11:03:28 +0530

    error

---
+++ game/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  game
+//
+//  Created by R87 on 01/03/23.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
--- game/FirstPage.swift
+++ game/FirstPage.swift
@@ -4,14 +4,13 @@ var QuestionArray  = ["1 + 1","2 + 6","3 + 9","5 + 3","8 + 2"]
 var QuestionRandom = QuestionArray.randomElement()
 var answerArray = [[2],[8],[12],[8],[10]]
 var answerRandom = answerArray.randomElement()
+
 class FirstPage: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+   
 
     var arrayForSell : [Int] = []
-   
-    
-    
+    @IBOutlet weak var cv: UICollectionView!
     var arr = Array<String>()
-    
     @IBOutlet weak var QuestionLabel: UILabel!
     
     override func viewDidLoad() {
@@ -21,37 +20,31 @@ class FirstPage: UIViewController,UICollectionViewDelegate,UICollectionViewDataS
             let random = Int.random(in :0...100)
             arrayForSell.append(random)
         }
-        
-        
-        
-        
-        
         print(QuestionRandom)
         QuestionLabel.text = "\(QuestionRandom!)"
-     
-            
         }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
-        
-        cell.AnswerLabel.text = ""
+        let cell = cv.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
         
         return cell
     }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 85, height: 85)
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+       
         
     }
     
-    }
+}
+    
+    
+
     
 
 
